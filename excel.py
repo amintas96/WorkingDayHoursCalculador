7f808f5f4e4b87b0c847014a63d5d380037ab9f5
amintas <amintaspereira@hotmail.com>
2022-07-08 17:56:10 -0300

    correção saida

--- excel.py
+++ excel.py
@@ -3,12 +3,19 @@ import pdf
 import CalculoDeHoras as ch
 
 disctHours = pdf.extrairPdf('relatorio (2).pdf')
-
+disctHours = {i: k for i , k in disctHours.items() if k != "Feriado" if k != "Folga" if k != "DSR" if k != "Compensação Dia" }
 listDates = list(disctHours.keys())
 listHours = list(disctHours.values())
 listHoursWorker = list(ch.calculaHorasDict(disctHours).values())
 
+
+print(listDates)
 dictWorker = {'Data': listDates,
-              'PontosBatidos':listHours}
+              'Pontos Batidos':listHours,
+               'Horas Trabalhadas':listHoursWorker }
 
 
+dictWorker = pd.DataFrame(dictWorker)
+
+file_name = 'HorasTrabalhadas.xlsx'
+dictWorker.to_excel(file_name)
